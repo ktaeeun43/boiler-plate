70716d17586fb90d2fcaf4e71d72bf6bf94acb71
ktaeeun43 <ktaeeun43@gmail.com>
2022-09-06 14:02:55 +0900

    logout

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ app.get('/api/users/auth', auth, (req, res) => {
   })
 })
 
+
 app.get('/api/users/logout', auth, (req, res) => {
   // console.log('req.user', req.user)
   User.findOneAndUpdate({ _id: req.user._id },
