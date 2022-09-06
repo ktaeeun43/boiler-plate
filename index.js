c653e608617caee3dd06d825c808c96ea2d96301
ktaeeun43 <ktaeeun43@gmail.com>
2022-09-06 09:44:43 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express()
 const port = 5000
-
+const { User } = require("./models/User")
+const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://ktaeeun:sf5595sf@cluster0.1gbdod7.mongodb.net/?retryWrites=true&w=majority", {
+const config = require("./config/key")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected...")).catch(err => console.log(err))
 
-app.get("/", (req, res) => res.send("연습용"))
+app.get("/", (req, res) => res.send("우왕"))
+app.post("/register", (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({success: true})
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
